c15f7464967f3460afdea75bb80891849c312ae9
David Cramer <dcramer@gmail.com>
2018-08-09 08:30:57 -0700

    Add sentry-sdk

--- cask/settings.py
+++ cask/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sentry_sdk
+
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://0cf873c1730740d496ec9e958911063f@sentry.io/1259509",
+    integrations=[DjangoIntegration()],
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
